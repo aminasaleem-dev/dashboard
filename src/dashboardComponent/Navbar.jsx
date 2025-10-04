e8e1f4da22936d49f678808050a04ab73fc0c21f
Amina Dev <amina.dev@gmail.com>
2025-10-04 17:49:31 +0500

    Update navbar heading

--- src/dashboardComponent/Navbar.jsx
+++ src/dashboardComponent/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Navbar = () => {
   return (
     <nav className="w-full bg-black p-4 rounded-lg text-white shadow-md mb-4">
-      <h1 className="text-xl font-bold">Welcome Amina!</h1>
+      <h1 className="text-xl font-bold">Welcome Amina saleem!</h1>
       <p className="text-sm opacity-80">Here's your Dashboard Overview</p>
     </nav>
   );
