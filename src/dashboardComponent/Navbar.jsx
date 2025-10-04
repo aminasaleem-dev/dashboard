262f796284615a9b0ee586f3d039dc21e959fd35
Amina Dev <amina.dev@gmail.com>
2025-10-04 18:31:23 +0500

    change navclr

--- src/dashboardComponent/Navbar.jsx
+++ src/dashboardComponent/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-    <nav className="w-full bg-black p-4 rounded-lg text-white shadow-md mb-4">
+    <nav className="w-full bg-gray-500 p-4 rounded-lg text-white shadow-md mb-4">
       <h1 className="text-xl font-bold">Welcome Amina saleem!</h1>
       <p className="text-sm opacity-80">Here's your Dashboard Overview</p>
     </nav>
